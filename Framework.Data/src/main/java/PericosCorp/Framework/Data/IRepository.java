5f8ca63e5200004a5e927d6ecbdc131ca63b9328
Arturo Salinas <arturo.salinas@stepstone.com>
2015-08-13 10:52:48 -0600

    Added functionallity of repository base and added functionallity on roleRepository

--- Framework.Data/src/main/java/PericosCorp/Framework/Data/IRepository.java
+++ Framework.Data/src/main/java/PericosCorp/Framework/Data/IRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public interface IRepository<T> {
 	public T Get(int id);
+	public Integer Save(T entity);
+	public void Update(T entity);
 	public void SaveUpdate(T entity);
-	public void Delete(int id);
+	public void Delete(T entity);
 	public List<T> GetAll();	
 }
--- Framework.Data/src/main/java/PericosCorp/Framework/Data/Repository.java
+++ Framework.Data/src/main/java/PericosCorp/Framework/Data/Repository.java
@@ -8,32 +8,33 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class Repository<T> implements IRepository<T> {
-	
-	protected Session sesion;
+public class Repository<T> implements IRepository<T> {	
+	protected Session session;
     protected Transaction tx;    
     
     protected void beginOperation() throws HibernateException
     {
-        sesion = HibernateUtil.getSessionFactory().openSession();
-        tx = sesion.beginTransaction();
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
     }
+    
     protected void manageException(Exception he) throws HibernateException
     {
         tx.rollback();
-        sesion.close();        
+        session.close();        
         throw new HibernateException("Ocurrió un error en la capa de acceso a datos", he);
     }
     
     protected void finishOperation()
     {
-        tx.commit();        
+        tx.commit();
     }
     
     @SuppressWarnings("unchecked")
 	private Class<T> getGenericClass() throws ClassNotFoundException{
         Class<T> inferedClass = null;
-        if(inferedClass == null){
+        if(inferedClass == null)
+        {
             Type mySuperclass = getClass().getGenericSuperclass();
             Type tType = ((ParameterizedType)mySuperclass).getActualTypeArguments()[0];
             String className = tType.toString().split(" ")[1];
@@ -45,30 +46,98 @@ public class Repository<T> implements IRepository<T> {
 	@SuppressWarnings("unchecked")
 	public T Get(int id) {		
 		beginOperation();
-        try {
-            return (T) sesion.get(getGenericClass(), id);
-        } catch (ClassNotFoundException ex) {
+        try 
+        {
+            return (T) session.get(getGenericClass(), id);
+        } 
+        catch (ClassNotFoundException ex) 
+        {
             //Logger.getLogger(RepositoryBase.class.getName()).log(Level.SEVERE, null, ex);
         }
-        finally{
-            sesion.close();
+        finally
+        {
+            session.close();
         }
         return null;
 	}
 
 	public void SaveUpdate(T entity) {
-		// TODO Auto-generated method stub
-		
+		try 
+        { 
+            beginOperation();
+            session.saveOrUpdate(entity);             
+        }
+		catch(Exception he) 
+        { 
+            manageException(he);            
+        }
+		finally 
+        { 
+            finishOperation();
+        } 		
 	}
 
-	public void Delete(int id) {
-		// TODO Auto-generated method stub
-		
+	public void Delete(T entity) {
+		try 
+        { 
+            beginOperation();
+            session.delete(entity);             
+        }
+		catch(HibernateException he) 
+        { 
+            manageException(he);
+            throw he; 
+        }
+		finally 
+        { 
+            finishOperation();
+        } 		
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<T> GetAll() {
-		// TODO Auto-generated method stub
-		return null;
+		 beginOperation();
+	     try 
+	     {
+	    	 return session.createCriteria(getGenericClass()).list();            
+	     }
+	     catch (ClassNotFoundException ex) 
+	     {
+	    	 //Logger.getLogger(RepositoryBase.class.getName()).log(Level.SEVERE, null, ex);
+	     }
+	     finally{
+	            session.close();
+	     }
+	     return null;
 	}
-
+	
+	public Integer Save(T entity) {
+		int id =0;
+        try 
+        { 
+            beginOperation();
+            id=(Integer) session.save(entity);             
+        }catch(Exception he) 
+        {         	
+            manageException(he);            
+        }finally 
+        { 
+            finishOperation();
+        } 
+        return id;
+	}
+	
+	public void Update(T entity) {
+		try 
+        { 
+            beginOperation();
+            session.update(entity);             
+        }catch(Exception he) 
+        { 
+            manageException(he);            
+        }finally 
+        { 
+            finishOperation();
+        }
+	}
 }
